aa8714e5c60cab7dfe6d8688b39a2ac3a3c5493a
weskern64 <prins.acosta@gmail.com>
2017-05-18 23:53:54 -0500

    Ejercicios Curso Java

--- src/main/java/com/kadasoftware/App.java
+++ src/main/java/com/kadasoftware/App.java
@@ -1,36 +1,13 @@
 package com.kadasoftware;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
     public static void main( String[] args ) {
 
-        Pato pato = new Pato() {
-            @Override
-            public String graznar() {
-                return null;
-            }
-        };
-
-        String name ="kasdasd";
-        name ="Hello there";
-
-        //System.out.printf(name);
-
-        //System.out.println( "Hello World!" );
-
-        //Collecciones collecciones = new Collecciones();
-
-        //collecciones.otroMapa();
-
         Mazo mazo = new Mazo();
         mazo.creaMazo();
         mazo.muestraMazo();
-
-
-
+        
     }
 }
